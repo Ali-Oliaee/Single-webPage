94635c2e7981559b7404cc7fc95bb58c32916547
Ali <79130209+ali80-sys@users.noreply.github.com>
2021-08-21 10:33:12 -0700

    Delete web directory

--- web/scripts/script.js
+++
@@ -1,8 +0,0 @@
-function moveMenu() {
-    let menu = document.getElementById('bar');
-    if (menu.classList.contains('hide'))
-        menu.classList.remove('hide');
-    else
-        menu.classList.add('hide');
-
-}
